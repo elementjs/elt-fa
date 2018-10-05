a58de4b8f85c612c2f01f1b9e04c2257eb6b254b
Christophe Eymard <christophe.eymard@gmail.com>
2018-10-05 23:13:24 +0200

    should now work better

--- mkicons.js
+++ mkicons.js
@@ -11,17 +11,12 @@ const OUT_DIR = path.join(__dirname, 'src')
  * @param {string} dir The directory to read
  * @param {string} suffix The suffix to add to a file
  */
-function get_files(dir) {
+function get_files(dir, suffix = '') {
   const files = fs.readdirSync(path.join(root, dir)).filter(s => s.match(/\.svg$/))
-  const base = path.basename(dir)
-  const out = path.join(OUT_DIR, base)
-
-  try {
-    fs.mkdirSync(out)
-  } catch (e) { }
+  const out = OUT_DIR
 
   for (var f of files) {
-    var dest = path.basename(f).replace(/\.svg$/, m => '.tsx')
+    var dest = path.basename(f).replace(/\.svg$/, m => suffix + '.tsx')
     const clsname = f.replace(/(?:^|-)([a-z])/g, (s, a) => a.toUpperCase())
       .replace(/\.svg$/, '')
     const source = fs.readFileSync(path.join(root, dir, f), {encoding: 'utf-8'})
@@ -31,17 +26,18 @@ function get_files(dir) {
     const src = `import { Attrs } from 'elt'
 import { Fa } from '../index'
 
-export default function ${clsname}(a: Attrs) {
+// ${clsname}
+export default function icon(a: Attrs) {
   return ${source}
 }
 `
 
-    // console.log(src)
+    // console.log(path.join(out, dest))
     fs.writeFileSync(path.join(out, dest), src, {encoding: 'utf-8'})
   }
 }
 
 get_files('svgs/brands')
-get_files('svgs/regular')
+get_files('svgs/regular', '-regular')
 get_files('svgs/solid')
 
