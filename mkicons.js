6aa0b8cdc1eea520ea0ce3ba1408931d5f8588cb
Christophe Eymard <christophe.eymard@gmail.com>
2018-10-05 23:04:49 +0200

    should now build correctly

--- mkicons.js
+++ mkicons.js
@@ -4,18 +4,24 @@ const path = require('path')
 const root = path.join(require.resolve('@fortawesome/fontawesome-free'), '../..')
 
 // const SVG_DIR = path.join(__dirname, '_svg')
-const OUT_DIR = path.join(__dirname)
+const OUT_DIR = path.join(__dirname, 'src')
 
 /**
  * ...
  * @param {string} dir The directory to read
  * @param {string} suffix The suffix to add to a file
  */
-function get_files(dir, suffix = '') {
+function get_files(dir) {
   const files = fs.readdirSync(path.join(root, dir)).filter(s => s.match(/\.svg$/))
+  const base = path.basename(dir)
+  const out = path.join(OUT_DIR, base)
+
+  try {
+    fs.mkdirSync(out)
+  } catch (e) { }
 
   for (var f of files) {
-    var dest = path.basename(f).replace(/\.svg$/, m => suffix + '.tsx')
+    var dest = path.basename(f).replace(/\.svg$/, m => '.tsx')
     const clsname = f.replace(/(?:^|-)([a-z])/g, (s, a) => a.toUpperCase())
       .replace(/\.svg$/, '')
     const source = fs.readFileSync(path.join(root, dir, f), {encoding: 'utf-8'})
@@ -23,20 +29,19 @@ function get_files(dir, suffix = '') {
       .replace(/<!--[^]*-->/m, '')
 
     const src = `import { Attrs } from 'elt'
-import { Fa } from './index'
+import { Fa } from '../index'
 
 export default function ${clsname}(a: Attrs) {
   return ${source}
 }
 `
-    // console.log(src)
-    fs.writeFileSync(path.join(OUT_DIR, dest), src, {encoding: 'utf-8'})
-    // console.log(OUT_DIR, dest)
 
+    // console.log(src)
+    fs.writeFileSync(path.join(out, dest), src, {encoding: 'utf-8'})
   }
 }
 
 get_files('svgs/brands')
-get_files('svgs/regular', '-regular')
+get_files('svgs/regular')
 get_files('svgs/solid')
 
