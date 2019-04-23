cf5778a711b67441985398801ed0e8131f7c538c
Christophe Eymard <christophe.eymard@sales-way.com>
2019-04-23 11:32:20 +0200

    elt-fa compiles on install instead of putting everything in npm

--- scripts/mkicons.js
+++ scripts/mkicons.js
@@ -1,10 +1,31 @@
 #!/usr/bin/env node
+/**
+ * Make the icons .js files as well as their .d.ts
+ */
+
+function try_require(pkg) {
+  try {
+    // The file returned is actually two folders up.
+    return path.join(require.resolve(pkg), '../..')
+  } catch (e) {
+    return null
+  }
+}
+
 const fs = require('fs')
 const path = require('path')
-const root = path.join(require.resolve('@fortawesome/fontawesome-free'), '../..')
+const root = try_require('@fortawesome/fontawesome-pro') || try_require('@fortawesome/fontawesome-free')
+
+if (!root) {
+  throw new Error(`Could not find any fontawesome package`)
+}
+
+const version = path.basename(root)
+console.log(`Compiling elt-fa for '${version}'`)
+
 
 // const SVG_DIR = path.join(__dirname, '_svg')
-const OUT_DIR = path.join(__dirname, 'src')
+const OUT_DIR = path.join(__dirname, '../src')
 
 /**
  * ...
@@ -12,7 +33,13 @@ const OUT_DIR = path.join(__dirname, 'src')
  * @param {string} suffix The suffix to add to a file
  */
 function get_files(dir, suffix = '') {
-  const files = fs.readdirSync(path.join(root, dir)).filter(s => s.match(/\.svg$/))
+  var files = []
+  try {
+    files = fs.readdirSync(path.join(root, dir)).filter(s => s.match(/\.svg$/))
+    console.log(`Making icons in ${dir} with suffix ${suffix}`)
+  } catch (e) {
+    return null
+  }
   const out = OUT_DIR
 
   for (var f of files) {
@@ -40,4 +67,4 @@ export default function icon(a: Attrs) {
 get_files('svgs/brands')
 get_files('svgs/regular', '-regular')
 get_files('svgs/solid')
-
+get_files('svgs/light', '-light')
