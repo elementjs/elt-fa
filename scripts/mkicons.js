c8cc15b862c71b003ec08659eab83802f7d18dc4
Christophe Eymard <christophe.eymard@sales-way.com>
2025-05-15 21:43:58 +0200

    fixes

--- scripts/mkicons.js
+++ scripts/mkicons.js
@@ -26,15 +26,17 @@ console.log(`building elt-fa for ${kind} ${version}`)
 
 fs.writeFileSync(path.join(__dirname, "../style.js"), `
 const st = document.createElement("style")
-st.textContent = \`.eltfa {
+st.textContent = \`
+.eltfa > svg {
+  height: 1em;
   color: currentcolor;
   height: 1em;
   vertical-align: -.125em;
 }
-.eltfa path {
+.eltfa > svg path {
   fill: currentcolor;
 }
-.eltfa .fa-secondary {
+.eltfa > svg .fa-secondary {
   opacity: 0.5;
 }
 \`
@@ -58,20 +60,20 @@ for (let dir of fs.readdirSync(path.join(root, "svgs"))) {
 import "./style"
 
 function _(svg) {
-  const tpl = document.createElement("template")
-  tpl.innerHTML = svg
-  const node = tpl.content.firstChild
+  const span = document.createElement("span")
+  span.className = "eltfa"
+  span.innerHTML = svg
   return function () {
-    return node.cloneNode(true)
+    return span.cloneNode(true)
   }
 }
 `)
 
   for (let icon of fs.readdirSync(path.join(root, "svgs", dir))) {
     const contents = fs.readFileSync(path.join(root, "svgs", dir, icon), "utf-8")
-      .replace(`xmlns="http://www.w3.org/2000/svg" `, `class="eltfa" `)
+      .replace(`xmlns="http://www.w3.org/2000/svg" `, "")
       .replace(/<!--[^]*-->/m, '')
-      .replace(/(<defs>)?<style>.fa-secondary{opacity:.4}<\/style>(<\/defs>)?/, '')
+      .replace(/(<defs>)?<style>.fa-secondary{opacity:.4}<\/style>(<\/defs>)?/, "")
 
     const fnname = "Fa" + mksnake(icon.replace(".svg", ""))
     res.push(`export let ${fnname} = /** @__PURE__ */ () => { const r = _(\`${contents}\`); ${fnname} = r; return r() }`)
@@ -81,63 +83,3 @@ function _(svg) {
   fs.writeFileSync(path.join(__dirname, `../${dir}.js`), res.join("\n"), "utf-8")
   fs.writeFileSync(path.join(__dirname, `../${dir}.d.ts`), dts.join("\n"), "utf-8")
 }
-
-// let match
-// while ((match = re_extract.exec(css))) {
-//   const [name, code] = match.slice(1)
-//   const snake_name = mksnake(name)
-//   // console.log(`export const Fa${snake_name} = "\\${code.length === 4 ? "u" : "x"}${code}",`)
-//   const faname = `Fa${snake_name}`
-//   res.push(`export const ${faname} = _("\\${code.length === 4 ? "u" : "x"}${code}")`)
-//   dts.push(`export function ${faname}(attrs?: Attrs): HTMLSpanElement`)
-// }
-
-// // console.log(dts.join("\n"))
-// fs.writeFileSync(path.join(__dirname, "../index.js"), res.join("\n"))
-// fs.writeFileSync(path.join(__dirname, "../index.d.ts"), dts.join("\n"))
-
-
-// // Brands are their own functions, so are duotones
-// const fonts = [
-//   ["sharp-solid", 900, "_fass", 700],
-//   ["solid", 900, "_fas"],
-//   ["regular", 400, "_far"],
-//   ["light", 300, "_fal"],
-//   ["thin", 100, "_fat"],
-// ]
-// for (let [bundle, weight, kls, weight2] of fonts) {
-//   const css = fs.readFileSync(path.join(root, `css/${bundle}.min.css`), "utf-8")
-//     .replace(/,url\([^)]*\) format\("truetype"\)/)
-//   const snake = mksnake(bundle)
-//   const fontFile = fs.readFileSync(path.join(root, `webfonts/fa-${bundle}-${weight}.woff2`))
-//   const data = "data:font/woff2;base64," + fontFile.toString("base64")
-
-//   // console.log(css)
-
-//   const fnname = `loadfont_${bundle.replace(/-/g, "_")}`
-
-//   const contents = `
-// const st = document.createElement("style")
-// st.textContent = \`
-// @font-face {
-// font-family: 'fontawesome';
-// font-style: normal;
-// font-weight: ${weight2 ?? weight};
-// font-display: block;
-// src: url("${data}") format("woff2"); }
-
-// ._fa {
-// font-family: 'fontawesome';
-// font-weight: ${weight2 ?? weight}; }
-// \`
-// document.head.appendChild(st)
-// `
-//   const out = path.join(__dirname, "../" + bundle + ".js")
-//   console.log("writing file ", out)
-//   fs.writeFileSync(out, contents, { encoding: "utf-8" })
-
-//   const outts = path.join(__dirname, "../" + bundle + ".d.ts")
-//   console.log("writing file ", outts)
-//   fs.writeFileSync(outts, "export const __fake: any", { encoding: "utf-8" })
-
-// }
